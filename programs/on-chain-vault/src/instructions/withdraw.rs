9885c905ca19cf5c9ab317533c9c7386fc0d1501
Richard Lavoura <richardlavoura@macbookpro.home>
2025-10-28 20:24:52 +0100

    feat: add withdraw instruction

--- programs/on-chain-vault/src/instructions/withdraw.rs
+++ programs/on-chain-vault/src/instructions/withdraw.rs
@@ -14,14 +14,43 @@ use anchor_lang::prelude::*;
 use crate::state::Vault;
 use crate::errors::VaultError;
 use crate::events::WithdrawEvent;
+use anchor_lang::solana_program::program::invoke;
+use anchor_lang::solana_program::system_instruction::transfer;
 
 #[derive(Accounts)]
+#[instruction(amount_to_withdraw: u64)]
 pub struct Withdraw<'info> {
     // TODO: Add required accounts and constraints
-    pub placeholder: Signer<'info>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut, 
+        constraint = vault.to_account_info().lamports() >= amount_to_withdraw @ VaultError::Overflow, 
+        constraint = vault.locked != true @ VaultError::VaultLocked
+        )]
+    pub vault: Account<'info, Vault>,
+    pub system_program: UncheckedAccount<'info>,
 }
 
 pub fn _withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
     // TODO: Implement withdraw functionality
-    todo!()
+    let ix = transfer(&ctx.accounts.vault.vault_authority.key(), &ctx.accounts.user.key(), amount);
+
+    invoke(
+        &ix,
+        &[
+            ctx.accounts.user.to_account_info(),
+            ctx.accounts.vault.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+        ],
+    )?;
+    
+    
+    emit!(WithdrawEvent{
+        amount: amount, 
+        vault: ctx.accounts.user.key(),
+        vault_authority: ctx.accounts.vault.vault_authority.key()
+    });
+
+    Ok(())
+
 }
