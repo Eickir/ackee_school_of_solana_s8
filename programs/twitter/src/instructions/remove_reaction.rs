b74052284403f085fcb5154c0ff0f8fdf8bb697a
Richard Lavoura <richardlavoura@macbookpro.home>
2025-11-03 20:46:38 +0100

    feat: add remove_reaction instruction

--- programs/twitter/src/instructions/remove_reaction.rs
+++ programs/twitter/src/instructions/remove_reaction.rs
@@ -15,14 +15,24 @@ use crate::errors::TwitterError;
 use crate::states::*;
 
 pub fn remove_reaction(ctx: Context<RemoveReactionContext>) -> Result<()> {
-    // TODO: Implement remove reaction functionality
-    todo!()
+    
+    let tweet = &mut ctx.accounts.tweet;
+    
+    match &ctx.accounts.tweet_reaction.reaction {
+        ReactionType::Like => tweet.likes -= 1,
+        ReactionType::Dislike => tweet.dislikes -= 1
+    }
+
+    Ok(())
 }
 
 #[derive(Accounts)]
 pub struct RemoveReactionContext<'info> {
     // TODO: Add required account constraints
+    #[account(mut)]
     pub reaction_author: Signer<'info>,
+    #[account(mut, close = reaction_author, has_one = reaction_author)]
     pub tweet_reaction: Account<'info, Reaction>,
+    #[account(mut)]
     pub tweet: Account<'info, Tweet>,
 }
